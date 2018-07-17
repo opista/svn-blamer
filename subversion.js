d2d8e44c6294d3fc9c4a50755138dfdfd0277e35
Beau August <beauagst@gmail.com>
2018-07-17 11:28:52 +0100

    feature: retrieving response as XML to prevent encoding errors

--- subversion.js
+++ subversion.js
@@ -1,6 +1,8 @@
 const vscode = require('vscode');
 const path = require('path');
 const child_process = require('child_process');
+const parseString = require('xml2js').parseString;
+const formatDate = require('./functions/formatDate');
 
 const subversion = {
     path: '',
@@ -51,16 +53,17 @@ const subversion = {
     getLog(revision) {
         if (Object.keys(this.revisions).length === 0) return;
         return new Promise((resolve) => {
-            const script = `svn log -r${revision} "${this.path}"`;
+            const script = `svn log -r${revision} "${this.path}" --xml`;
             child_process.exec(script, (error, stdout, stderr) => {
-                if (error) vscode.window.showErrorMessage(stderr);
-                else {
-                    const log = (stdout).split(/\n/);
-                    const email = log[1].split(" | ")[1];
-                    const date = log[1].match(/[^()]+(?=\))/)[0];
-                    const message = log.slice(3, -2);
+                if (error) return vscode.window.showErrorMessage(stderr);
+
+                parseString(stdout, (err, result) => {
+                    const data = result.log.logentry[0];
+                    const email = data.author[0];
+                    const date = formatDate(data.date[0]);
+                    const message = data.msg[0];
                     resolve({ email, date, message });
-                }
+                });         
             });
         });
     },
