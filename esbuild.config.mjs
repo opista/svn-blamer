3a1ad21f5c40beda2743cdc42ac4a95a2c9580e0
BeauAgst <10343831+BeauAgst@users.noreply.github.com>
2024-03-29 23:34:44 +0000

    chore: Fixes bug with tasks not being configured (#484)
    Since switching from webpack to esbuild, there was no problem matcher
    defined for the watch task. This defines it, and allows the task to run
    as expected again.

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,30 +1,37 @@
 import * as esbuild from "esbuild";
 import { copy } from "esbuild-plugin-copy";
 
-(async () => {
-    const res = await esbuild.build({
-        bundle: true,
-        entryPoints: ["./src/extension.ts"],
-        external: ["vscode"],
-        format: "cjs",
-        minify: true,
-        outfile: "./dist/extension.js",
-        platform: "node",
-        plugins: [
-            copy({
-                resolveFrom: "cwd",
-                assets: {
-                    from: ["./src/img/**/*"],
-                    to: ["./dist/img"],
-                },
-            }),
-            copy({
-                resolveFrom: "cwd",
-                assets: {
-                    from: ["./public/**/*"],
-                    to: ["./dist/public"],
-                },
-            }),
-        ],
-    });
-})();
+let ctx = await esbuild.context({
+    bundle: true,
+    entryPoints: ["./src/extension.ts"],
+    external: ["vscode"],
+    format: "cjs",
+    logLevel: "info",
+    minify: process.argv.includes("--minify"),
+    outfile: "./dist/extension.js",
+    platform: "node",
+    plugins: [
+        copy({
+            resolveFrom: "cwd",
+            assets: {
+                from: ["./src/img/**/*"],
+                to: ["./dist/img"],
+            },
+        }),
+        copy({
+            resolveFrom: "cwd",
+            assets: {
+                from: ["./public/**/*"],
+                to: ["./dist/public"],
+            },
+        }),
+    ],
+    sourcemap: process.argv.includes("--sourcemap"),
+});
+
+if (process.argv.includes("--watch")) {
+    await ctx.watch();
+} else {
+    await ctx.rebuild();
+    await ctx.dispose();
+}
