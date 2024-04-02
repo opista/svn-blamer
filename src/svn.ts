784b5b901a4b76be980d212ebb39f0a8e074d02a
BeauAgst <10343831+BeauAgst@users.noreply.github.com>
2024-04-02 10:42:12 +0100

    chore: Refactor svn methods to use shared command method

--- src/svn.ts
+++ src/svn.ts
@@ -11,66 +11,56 @@ import { spawnProcess } from "./util/spawn-process";
 export class SVN {
     constructor(private logger: LogOutputChannel) {}
 
-    private async command(command: string) {
-        const { svnExecutablePath } = workspace.getConfiguration(EXTENSION_CONFIGURATION);
+    private async command(command: string, fileName: string) {
+        try {
+            const { svnExecutablePath } = workspace.getConfiguration(EXTENSION_CONFIGURATION);
 
-        if (!svnExecutablePath) {
-            throw new ConfigurationError(
-                `${EXTENSION_CONFIGURATION}.svnExecutablePath`,
-                svnExecutablePath,
-            );
+            if (!svnExecutablePath) {
+                throw new ConfigurationError(
+                    `${EXTENSION_CONFIGURATION}.svnExecutablePath`,
+                    svnExecutablePath,
+                );
+            }
+
+            return await spawnProcess(`${svnExecutablePath} ${command}`);
+        } catch (err: any) {
+            if (typeof err === "string") {
+                if (err.includes("E155007")) {
+                    this.logger.warn("File is not a working copy, cannot complete action");
+                    throw new NotWorkingCopyError(fileName);
+                }
+
+                throw new Error(err);
+            }
+
+            throw err;
         }
-
-        return await spawnProcess(`${svnExecutablePath} ${command}`);
     }
 
     async blameFile(fileName: string): Promise<Blame[]> {
         this.logger.debug("Running blame child process");
         try {
-            const data = await this.command(`blame --xml -x "-w --ignore-eol-style" "${fileName}"`);
+            const data = await this.command(
+                `blame --xml -x "-w --ignore-eol-style" "${fileName}"`,
+                fileName,
+            );
 
             this.logger.debug("Blame child process successful");
 
             return mapBlameOutputToBlameModel(data);
         } catch (err: any) {
-            if (typeof err === "string" && err.includes("E155007")) {
-                this.logger.warn("File is not a working copy, cannot complete action");
-                throw new NotWorkingCopyError(fileName);
-            }
-
             this.logger.error("Failed to blame file", { err });
-            throw new Error(err);
+            throw err;
         }
     }
 
     async getLogForRevision(fileName: string, revision: string) {
-        const data = await this.command(`log --xml -r ${revision} "${fileName}"`);
-
-        return mapLogOutputToMessage(data);
-    }
-
-    async getLogsForRevisions(fileName: string, revisions: string[]): Promise<Log[]> {
-        this.logger.debug("Running log child process");
-
         try {
-            const logs = await Promise.all(
-                revisions.map(async (revision) => {
-                    const log = await this.getLogForRevision(fileName, revision);
-                    return { log, revision };
-                }),
-            );
-
-            this.logger.debug("Log child process successful");
-
+            const data = await this.command(`log --xml -r ${revision} "${fileName}"`, fileName);
             return mapLogOutputToMessage(data);
         } catch (err: any) {
-            if (typeof err === "string" && err.includes("E155007")) {
-                this.logger.warn("File is not a working copy, cannot complete action");
-                throw new NotWorkingCopyError(fileName);
-            }
-
             this.logger.error("Failed to get revision log", { err });
-            throw new Error(err);
+            throw err;
         }
     }
 }
