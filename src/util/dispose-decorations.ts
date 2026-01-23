5a413b046506837ef7c3483b9bf27242d5f3073e
BeauAgst <10343831+metacurb@users.noreply.github.com>
2026-01-23 10:22:25 +0000

    formatting

--- src/util/dispose-decorations.ts
+++ src/util/dispose-decorations.ts
@@ -1,6 +1,8 @@
 import { TextEditorDecorationType } from "vscode";
 
-export const disposeDecorations = async (decorations: TextEditorDecorationType[]): Promise<void> => {
+export const disposeDecorations = async (
+    decorations: TextEditorDecorationType[],
+): Promise<void> => {
     const chunkSize = 100;
     for (let i = 0; i < decorations.length; i += chunkSize) {
         const chunk = decorations.slice(i, i + chunkSize);
