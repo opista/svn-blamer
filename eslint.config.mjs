db022aabb3ac8b109897aa57c3062536e30b7999
BeauAgst <10343831+metacurb@users.noreply.github.com>
2026-02-02 10:51:54 +0000

    linting and formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,50 +1,57 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import unusedImports from "eslint-plugin-unused-imports";
-import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import { defineConfig, globalIgnores } from "eslint/config";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    allConfig: js.configs.all,
 });
 
-export default defineConfig([globalIgnores(["**/out", "**/dist", "**/*.d.ts", "**/.github"]), {
-    extends: compat.extends("prettier"),
+export default defineConfig([
+    globalIgnores(["**/out", "**/dist", "**/*.d.ts", "**/.github"]),
+    {
+        extends: compat.extends("prettier"),
 
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-        "simple-import-sort": simpleImportSort,
-        "unused-imports": unusedImports,
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+            "simple-import-sort": simpleImportSort,
+            "unused-imports": unusedImports,
+        },
+
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 6,
+            sourceType: "module",
+        },
+
+        rules: {
+            "@typescript-eslint/naming-convention": [
+                "warn",
+                {
+                    selector: "import",
+                    format: ["camelCase", "PascalCase"],
+                },
+            ],
+
+            "@typescript-eslint/semi": "warn",
+            curly: "warn",
+            eqeqeq: "warn",
+            "no-throw-literal": "warn",
+            semi: "off",
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
+            "unused-imports/no-unused-imports": "error",
+            "no-console": "error",
+        },
     },
-
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 6,
-        sourceType: "module",
-    },
-
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        "@typescript-eslint/semi": "warn",
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "off",
-        "simple-import-sort/imports": "error",
-        "simple-import-sort/exports": "error",
-        "unused-imports/no-unused-imports": "error",
-        "no-console": "error",
-    },
-}]);
+]);
