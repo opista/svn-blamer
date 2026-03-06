2f54d31fe94bd63fdceec0f607ebfd917b55b95e
metacurb <10343831+metacurb@users.noreply.github.com>
2026-03-06 15:29:13 +0000

    refactor: set prototype for SvnCommandError explicitly
    Added `Object.setPrototypeOf(this, SvnCommandError.prototype)` to the `SvnCommandError` constructor to ensure proper prototype chain establishment and make `instanceof` checks work reliably in environments like ES5.

--- src/errors/svn-command-error.ts
+++ src/errors/svn-command-error.ts
@@ -2,5 +2,6 @@ export class SvnCommandError extends Error {
     constructor(message: string) {
         super(message);
         this.name = "SvnCommandError";
+        Object.setPrototypeOf(this, SvnCommandError.prototype);
     }
 }
